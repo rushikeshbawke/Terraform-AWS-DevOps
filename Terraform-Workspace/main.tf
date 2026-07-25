6f25f5ee0056b49d3f67fbb415f73f0139827d35
rushikeshbawke <rushikeshbawke0000@gmail.com>
2026-07-26 01:11:04 +0530

    run the workspaces succesfully. feeling good thanks for reading all this.

--- Terraform-Workspace/main.tf
+++ Terraform-Workspace/main.tf
@@ -9,3 +9,10 @@ resource "aws_instance" "my-server" {
 locals {
   instance_name = "${terraform.workspace}-server"
 }
+
+# $ terraform workspace list => shows all workspaces present
+# $ terraform workspace new dev => it will add new workspace and switch the added one directly
+# $ terraform workspace select dev => it will switch the workspace to dev
+# $ terraform workspace show => it will show the active workspace
+# $ i created dev.tfvars, test.tfvars, prod.tfvars which are holding variables location and instance_type
+# $ then pass it like this $ terraform apply -var-file="dev.tfvars"
--- Terraform-Workspace/variables.tf
+++ Terraform-Workspace/variables.tf
@@ -1,11 +1,9 @@
 variable "location" {
   type        = string
   description = "Region of instance"
-  default     = "ap-south-1"
 }
 
 variable "instance_type" {
   type        = string
   description = "type of instance"
-  default     = "t3.micro"
 }
